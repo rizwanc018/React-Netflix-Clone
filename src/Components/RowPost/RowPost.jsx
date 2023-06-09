9f3be81a126e99bd4959b114f6750883a9c4c716
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-06-09 10:43:36 +0530

    trilers added

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,30 +1,60 @@
 import { useEffect, useState } from 'react'
+import YouTube from 'react-youtube';
 import './RowPost.css'
 import axios from '../../axios'
 
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [trailerId, setTrailerId] = useState('')
+
   useEffect(() => {
-    axios.get(props.url)
+    axios.get(props.url).then(response => {
+      setMovies(response.data.results)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  // options for react-youtube
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    }
+  }
+
+  const showTrailer = (id) => {
+    axios.get(`/movie/${id}/videos?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US`)
       .then(response => {
-        setMovies(response.data.results)
+        if (response.data.results.length){
+          console.log(response.data.results);
+          setTrailerId(response.data.results[0])
+        }
+        else console.log('>>>>>>>>>> No Results');
       }).catch(err => {
         console.log(err)
       })
-  }, [])
+  }
 
   return (
-    <div className='row'>
+    <div className='row' >
       <h2>{props.title}</h2>
       <div className='posters'>
         {
           // eslint-disable-next-line react/jsx-key
-          movies.map((movie) => <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${import.meta.env.VITE_IMAGE_URL + movie.backdrop_path}`} alt="posters" />
-
+          movies.map((movie) =>
+            <img
+              onClick={() => showTrailer(movie.id)}
+              className={props.isSmall ? 'smallPoster' : 'poster'}
+              src={`${import.meta.env.VITE_IMAGE_URL + movie.backdrop_path}`} alt="posters"
+            />
           )
         }
       </div>
-    </div>
+      {trailerId && <YouTube videoId={trailerId.key} opts={opts} />}
+    </div >
   )
 }
 
